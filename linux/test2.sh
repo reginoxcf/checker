bf8502d46c52fea7b84885d0a145b0b46b571ddb
reginox <99781653+reginoxcf@users.noreply.github.com>
2026-02-13 11:18:16 +0700

    Enhance test2.sh with compilation flags and timing
    Updated compilation command and added timing for execution.

--- linux/test2.sh
+++ linux/test2.sh
@@ -1,3 +1,5 @@
-clear 
-g++ b.cpp -o b  || { echo "Compile Error"; exit 1; }
-time ./b < test.inp > test.ans
+g++ b.cpp -std=c++17 -lm -O2 -o b  || { echo "Compile Error"; exit 1; }
+# /usr/bin/time -f "Time: %Us" ./b < test.inp > test.ans
+ts=$(date +%s%N)
+./b < test.inp > test.ans
+echo $'Time:' $((($(date +%s%N) - $ts)/1000000)) $'ms'

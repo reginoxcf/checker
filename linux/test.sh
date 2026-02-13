ccfe55079519ed05f53b6975a26cdaed14fc5a51
reginox <99781653+reginoxcf@users.noreply.github.com>
2026-02-13 11:17:43 +0700

    Modify test script for C++17 and execution timing
    Updated compilation flags and added timing for execution.

--- linux/test.sh
+++ linux/test.sh
@@ -1,3 +1,5 @@
-clear
-g++ a.cpp -O2 -o a  || { echo "Compile Error"; exit 1; }
-time ./a < test.inp > test.out
+g++ a.cpp -std=c++17 -lm -O2 -o a  || { echo "Compile Error"; exit 1; }
+# /usr/bin/time -f "Time: %Us" ./a < test.inp > test.out
+ts=$(date +%s%N)
+./a < test.inp > test.out
+echo $'Time:' $((($(date +%s%N) - $ts)/1000000)) $'ms'
